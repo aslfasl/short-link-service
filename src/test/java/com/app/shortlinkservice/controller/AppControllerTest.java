f7e7db58035ab44cd9ca72f77e202870d0c9a081
VolkovAndrey <soupec@yandex.ru>
2022-11-27 18:35:47 +0300

    Add mock of repo in controller testing

--- src/test/java/com/app/shortlinkservice/controller/AppControllerTest.java
+++ src/test/java/com/app/shortlinkservice/controller/AppControllerTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
-
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -28,8 +26,6 @@ class AppControllerTest {
     @MockBean
     private LinkService mockLinkService;
 
-//    @Autowired
-//    private LinkRepo linkRepo;
 
     @Test
     void createShortLink() throws Exception {
