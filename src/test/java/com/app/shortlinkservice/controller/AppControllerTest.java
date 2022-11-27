002c40fa0d33885aa67ebc2c704a06dabf206faa
VolkovAndrey <soupec@yandex.ru>
2022-11-27 18:35:17 +0300

    Add mock of repo in controller testing

--- src/test/java/com/app/shortlinkservice/controller/AppControllerTest.java
+++ src/test/java/com/app/shortlinkservice/controller/AppControllerTest.java
@@ -2,17 +2,18 @@ package com.app.shortlinkservice.controller;
 
 import com.app.shortlinkservice.entity.ShortLink;
 import com.app.shortlinkservice.repository.LinkRepo;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.app.shortlinkservice.service.LinkService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,29 +25,35 @@ class AppControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private LinkRepo linkRepo;
+    @MockBean
+    private LinkService mockLinkService;
+
+//    @Autowired
+//    private LinkRepo linkRepo;
 
     @Test
     void createShortLink() throws Exception {
         String longValue = "testingMethodWithThisString";
+        String shortValue = "1234";
+        ShortLink shortLink = new ShortLink();
+        shortLink.setLongValue(longValue);
+        shortLink.setShortValue(shortValue);
+        when(mockLinkService.createShortLink(longValue)).thenReturn(shortLink);
+
+
         mockMvc.perform((post("/short-link-service/links/create"))
                         .content(longValue))
                 .andExpect(status().isCreated())
                 .andDo(print())
-                .andExpect(jsonPath("$.longValue", equalTo(longValue)));
+                .andExpect(jsonPath("$.longValue", equalTo(longValue)))
+                .andExpect(jsonPath("$.shortValue", equalTo(shortValue)));
     }
 
     @Test
     void getOriginalUrl() throws Exception {
         String shortValue = "1234";
         String longValue = "zzzzzzzzz";
-        ShortLink entity = new ShortLink();
-        entity.setShortValue(shortValue);
-        entity.setLongValue(longValue);
-        entity.setCreationTime(LocalDateTime.now());
-        entity.setLastCallTime(LocalDateTime.now());
-        linkRepo.save(entity);
+        when(mockLinkService.getLongValueByShortValue(shortValue)).thenReturn(longValue);
 
         mockMvc.perform(get("/short-link-service/links/get/" + shortValue))
                 .andExpect(status().isOk())
